45df36866834d024df31b1aa717733af0ca99d17
Radhika Raghu <RadhikaRaghu@Radhikas-MacBook-Air.local>
2016-04-09 01:29:58 +0530

    added drinks and dess

--- public/index.php
+++ public/index.php
@@ -17,13 +17,6 @@ session_start();
 $settings = require __DIR__ . '/../src/settings.php';
 $app = new \Slim\App($settings);
 
-    $app->get('/hello/[{name}]', function ($request, $response, $args) {
-        $name = $request->getAttribute('name');
-        $response->getBody()->write("Hello, $name");
-
-        return $response;
-    });
-
     $app->get('/menu/f', function ($request, $response, $args) {
         $servername = "localhost";
         $username = "root";
@@ -57,17 +50,70 @@ $app = new \Slim\App($settings);
         $conn->close();
     });
 
-    $app->get('/{id}', function ($request, $response, $args) {
-    $id = $request->getAttribute('id');
+    $app->get('/menu/dr', function ($request, $response, $args) {
+        $servername = "localhost";
+        $username = "root";
+        $password = "root";
+        $dbname = "Menu";
+        $conn = new mysqli($servername, $username, $password, $dbname);
 
-    return $response->withJSON(
-        ['id' => $id]
-    );
+        // Create connection
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } 
+
+        $sql = "SELECT * FROM `DRINKS`";
+        $result = $conn->query($sql);
+        $rows = array();
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($r = $result->fetch_assoc()) {
+                $rows[] = $r;
+                
+            }
+        } 
+        else {
+            echo "0 results";
+        }
+        
+
+        echo json_encode($rows);
+        $conn->close();
+    });
+
+     $app->get('/menu/dess', function ($request, $response, $args) {
+        $servername = "localhost";
+        $username = "root";
+        $password = "root";
+        $dbname = "Menu";
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        // Create connection
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } 
+
+        $sql = "SELECT * FROM `DESS`";
+        $result = $conn->query($sql);
+        $rows = array();
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($r = $result->fetch_assoc()) {
+                $rows[] = $r;
+                
+            }
+        } 
+        else {
+            echo "0 results";
+        }
+        
+
+        echo json_encode($rows);
+        $conn->close();
     });
 
 $app->run();
-
-
-
-
-
