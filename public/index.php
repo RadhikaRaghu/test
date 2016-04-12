25cdcc8e75cd08e7ac30a13b05246396177492ff
Radhika Raghu <RadhikaRaghu@Radhikas-MacBook-Air.local>
2016-04-12 23:22:32 +0530

    Manager

--- public/index.php
+++ public/index.php
@@ -155,7 +155,36 @@ $dbname = "test";
         $conn->close();
     });
 
-     
+    $app->get('/managerR', function ($request, $response, $args) {
+        
+        global $servername, $username, $password, $dbname;
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        // Create connection
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } 
+
+        $sql = "SELECT * FROM `Reservations`";
+        $result = $conn->query($sql);
+        $rows = array();
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($r = $result->fetch_assoc()) {
+                $rows[] = $r;
+                
+            }
+        } 
+        else {
+            echo "0 results";
+        }
+        
+
+        echo json_encode($rows);
+        $conn->close();
+    }); 
 
 
 $app->run();
