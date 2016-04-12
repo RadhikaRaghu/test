0ceb99e1e37b2a0dfd8a05507f87d29265c46450
Radhika Raghu <RadhikaRaghu@Radhikas-MacBook-Air.local>
2016-04-12 22:37:49 +0530

    cid

--- public/index.php
+++ public/index.php
@@ -149,9 +149,38 @@ $dbname = "menu";
         $conn->close();
     });
 
+     $app->get("/reserve/CID", function (Request $request, Response $response) {
+        
+        global $servername, $username, $password, $dbname;
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Create connection
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } 
+
+        $sql = "SELECT CID FROM `Reservations`";
+        $result = $conn->query($sql);
+        $rows = array();
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($r = $result->fetch_assoc()) {
+                $rows[] = $r;
+                
+            }
+        } 
+        else {
+            echo "0 results";
+        }
+        
+
+        echo json_encode($rows);
+        $conn->close();
+    });
+
+
 $app->run();
-
-
 
 
 
