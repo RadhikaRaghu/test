46349a36aa1f832e55d99384ff7373f8e3b12c3c
Radhika Raghu <RadhikaRaghu@Radhikas-MacBook-Air.local>
2016-04-20 16:11:24 +0530

    weekly sales

--- public/index.php
+++ public/index.php
@@ -186,6 +186,7 @@ $dbname = "test";
         echo json_encode($rows);
         $conn->close();
     }); 
+
     $app->get('/manager/WeeklySales', function ($request, $response, $args) {
         
         global $servername, $username, $password, $dbname;
@@ -226,6 +227,37 @@ $dbname = "test";
             }
         }         
         echo json_encode($output);
+        $conn->close();
+    }); 
+
+    $app->get('/manager/EmpShift', function ($request, $response, $args) {
+        
+        global $servername, $username, $password, $dbname;
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        // Create connection
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } 
+
+        $sql = "SELECT * FROM `EmployeeShifts`";
+        $result = $conn->query($sql);
+        $rows = array();
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($r = $result->fetch_assoc()) {
+                $rows[] = $r;
+                
+            }
+        } 
+        else {
+            echo "0 results";
+        }
+        
+
+        echo json_encode($rows);
         $conn->close();
     }); 
 
